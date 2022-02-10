6463731404f8e43889879a0df33ccb60f45c754b
Dick Davis <dick@hey.com>
2022-02-09 20:11:51 -0600

    Add basic preferences activity

--- app/src/main/java/com/example/bobspizza/MainActivity.kt
+++ app/src/main/java/com/example/bobspizza/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.bobspizza
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.example.bobspizza.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.preferences_menu_item) {
+            val intent = Intent(this, PreferencesActivity::class.java)
+            startActivity(intent)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
