3286f31df8085c86dd17eb805623c6acfb546db6
Dick Davis <dick@hey.com>
2022-02-10 21:19:36 -0600

    Apply user preferences to main activity

--- app/src/main/java/com/example/bobspizza/MainActivity.kt
+++ app/src/main/java/com/example/bobspizza/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.bobspizza
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import com.example.bobspizza.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +18,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val preferences = getSharedPreferences(getString(R.string.pref_key), MODE_PRIVATE)
+        val bgColorPreference = preferences.getString(
+            getString(R.string.bg_color_key),
+            getString(R.string.default_bg_color)
+        )
+
+        val bgColor = when(bgColorPreference) {
+            getString(R.string.red) -> Color.parseColor("#fab1a0")
+            getString(R.string.blue) -> Color.parseColor("#83ecec")
+            getString(R.string.green) -> Color.parseColor("#55efc4")
+            else -> Color.WHITE
+        }
+
+        binding.mainLayout.setBackgroundColor(bgColor)
+
+        val useLargeText = preferences.getBoolean(getString(R.string.text_size_key), false)
+        val textSize = if (useLargeText) 24.0f else 16.0f
+        val textViewIds = listOf(
+            R.id.label_background_color, R.id.label_crust, R.id.label_delivery_method,
+            R.id.label_drinks, R.id.label_num_pizzas, R.id.label_tip, R.id.label_toppings
+        )
+
+        for(id in textViewIds) {
+            val textview : TextView? = binding.root.findViewById(id)
+            textview?.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
